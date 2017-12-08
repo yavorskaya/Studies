5a7808089d3922550161c62595e982a381b8239a
Yavorskaya <taffytf@gmail.com>
2017-12-08 13:38:27 +0300

    Functions were added

--- Chapter8/IntergersPrint/IntergersPrint/Source.cpp
+++ Chapter8/IntergersPrint/IntergersPrint/Source.cpp
@@ -14,12 +14,33 @@ void Print(const vector<int>& _vec, string _str)
 		cout << _vec[i] << _str;
 	cout << _vec[(_vec.size() - 1)];
 	cout << endl;
-	
+}
+
+void Print(const vector<int>& _vec, string _name, string _str)
+{
+	cout << _name << "\n";
+	for (int i = 0; i < (_vec.size()); ++i) {
+		if (i != (_vec.size() - 1))
+			cout << _vec[i] << _str; // После последнего элемента не должен выводиться символ разделения
+		else cout << _vec[i];
+	}
+	cout << endl;
+}
+
+void PrintUsingRange(const vector<int>& _vec, string _str)
+{
+	cout << _str << endl;
+	for (int x : _vec)
+		cout << x << " ";
+	cout << endl;
+
 }
 
 int main(int argc, char* argv[])
 {
 	vector<int>vec = {3, 7, 9, 10, 11, 56, 2, 90, 33};
 	Print(vec, ", ");
+	PrintUsingRange(vec, "Vector with range: ");
+	Print(vec, "Function Overload: ", ", ");
 	return 0;
 }
